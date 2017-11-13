432d51dc328ca81a3ef7a6c196c1f0d153a51c02
ishandas387 <ishan.das387@gmail.com>
2017-11-14 00:25:52 +0530

    orders table

--- app/src/main/java/com/ishan387/testlogin/Cart.java
+++ app/src/main/java/com/ishan387/testlogin/Cart.java
@@ -1,17 +1,28 @@
 package com.ishan387.testlogin;
 
+import android.content.DialogInterface;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.ishan387.testlogin.com.ishan387.db.CartDatabase;
 import com.ishan387.testlogin.model.CartAdapter;
 import com.ishan387.testlogin.model.CartItems;
+import com.ishan387.testlogin.model.Orders;
+import com.ishan387.testlogin.model.Product;
 
 import org.w3c.dom.Text;
 
@@ -29,6 +40,9 @@ public class Cart extends AppCompatActivity {
     TextView total;
     List<CartItems> itemsInCart = new ArrayList<>();
     CartAdapter adapter;
+    Button placeOrder;
+    List<Product> productList = new ArrayList<>();
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +56,80 @@ public class Cart extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-
+        placeOrder = (Button) findViewById(R.id.placeorder);
         total = (TextView) findViewById(R.id.total);
+        mAuth = FirebaseAuth.getInstance();
         loadList();
+
+        placeOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+                Orders o = new  Orders();
+               // o.setUserPhoneNumber(editText.getText().toString());
+                o.setOrderId(String.valueOf(System.currentTimeMillis()));
+                if(currentUser!=null)
+                {
+                    o.setUserName(currentUser.getDisplayName());
+                }
+                o.setProducts(productList);
+                Float tot =0.0f;
+                for(Product pt : productList)
+                {
+                    tot += pt.getPrice();
+                }
+                o.setTotal(tot.toString());
+                orderrequest.child(o.getOrderId()).setValue(o);
+                new CartDatabase(getBaseContext()).cleanCart();
+                Toast.makeText(Cart.this,"Order placed",Toast.LENGTH_LONG).show();
+                finish();
+             //  showAlertDialog();
+            }
+        });
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Cart.this);
+        alertDialog.setTitle("One more step");
+        alertDialog.setMessage("Add your phone number");
+        final EditText editText = new EditText(Cart.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
+        editText.setLayoutParams(lp);
+        alertDialog.setView(editText);
+
+        alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+               if(null != editText.getText() && !editText.getText().toString().isEmpty())
+               {
+                   Orders o = new  Orders();
+                   o.setUserPhoneNumber(editText.getText().toString());
+                   o.setOrderId(String.valueOf(System.currentTimeMillis()));
+                   o.setProducts(productList);
+                   Float tot =0.0f;
+                   for(Product pt : productList)
+                   {
+                       tot += pt.getPrice();
+                   }
+                   o.setTotal(tot.toString());
+                   orderrequest.child(o.getTimeStamp()).setValue(o);
+                   new CartDatabase(getBaseContext()).cleanCart();
+                   Toast.makeText(Cart.this,"Order placed",Toast.LENGTH_LONG).show();
+               }
+               else
+               {
+                   showAlertDialog();
+               }
+            }
+        });
+
+        alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
     }
 
     private void loadList() {
@@ -56,6 +141,11 @@ public class Cart extends AppCompatActivity {
         float t = 0.0f;
         for(CartItems item : itemsInCart)
         {
+            Product p = new Product();
+            p.setPrice(Float.parseFloat(item.getPrice()));
+            p.setName(item.getProductName());
+
+            productList.add(p);
             t += Float.parseFloat(item.getPrice());
         }
         Locale local = new Locale("en","IN");
--- app/src/main/java/com/ishan387/testlogin/model/Orders.java
+++ app/src/main/java/com/ishan387/testlogin/model/Orders.java
@@ -1,34 +1,96 @@
 package com.ishan387.testlogin.model;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by ishan on 04-11-2017.
  */
 
-class Orders {
+public class Orders {
 
-    Date date;
+    String timeStamp;
     String orderId;
     String productId;
     String userName;
     String uid;
-    String price;
-    String productName;
+    List<Product> products;
     String serviceTime;
+    String total;
+
+    public Orders(String timeStamp, String orderId, String productId, String userName, String uid, List<Product> products, String serviceTime, String total, String userPhoneNumber) {
+        this.timeStamp = timeStamp;
+        this.orderId = orderId;
+        this.productId = productId;
+        this.userName = userName;
+        this.uid = uid;
+        this.products = products;
+        this.serviceTime = serviceTime;
+        this.total = total;
+        this.userPhoneNumber = userPhoneNumber;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    String userPhoneNumber;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public String getServiceTime() {
+        return serviceTime;
+    }
+
+    public void setUserName(String userName) {
+
+        this.userName = userName;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public void setServiceTime(String serviceTime) {
+        this.serviceTime = serviceTime;
+    }
+
+
+    public void setUserPhoneNumber(String userPhoneNumber) {
+        this.userPhoneNumber = userPhoneNumber;
+    }
+
+    public String getUserPhoneNumber() {
+        return userPhoneNumber;
+    }
+
+
 
     public Orders() {
     }
 
-    public Orders(Date date, String orderId, String productId) {
 
-        this.date = date;
-        this.orderId = orderId;
-        this.productId = productId;
-    }
 
-    public void setDate(Date date) {
-        this.date = date;
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
     }
 
     public void setOrderId(String orderId) {
@@ -39,8 +101,8 @@ class Orders {
         this.productId = productId;
     }
 
-    public Date getDate() {
-        return date;
+    public String getTimeStamp() {
+        return timeStamp;
     }
 
     public String getOrderId() {
